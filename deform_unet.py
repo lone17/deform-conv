3d11183e9044d2cf711b8ab1b637887347f2fb5d
ian <ian@cinnamon.is>
2020-03-06 12:24:37 +0700

    update training script

--- deform_unet.py
+++ deform_unet.py
@@ -1,4 +1,4 @@
-import random
+from functools import partial
 
 import keras
 import numpy as np
@@ -6,10 +6,8 @@ from keras.models import *
 from keras.layers import *
 from keras import backend as K
 from keras.optimizers import *
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 
 from utils import *
-from load_data import data_generator
 from deform_conv.layers import ConvOffset2D
 
 def create_weighted_binary_crossentropy(zero_weight, one_weight):
@@ -54,84 +52,67 @@ def IoU_score(y_true, y_pred, smooth=1e-6):
     return iou
 
 def Conv(input, num_filters, use_deform=False, activation='relu', padding='same', 
-         kernel_initializer='he_normal', normal_conv_trainable=True):
+         kernel_initializer='he_normal', normal_conv_trainable=True,
+         channel_wise=False):
     input = Conv2D(num_filters, (3, 3), activation=activation, padding=padding, 
                    kernel_initializer=kernel_initializer, 
                    trainable=normal_conv_trainable)(input)
     if use_deform:
-        input = ConvOffset2D(num_filters, channel_wise=False)(input)
+        input = ConvOffset2D(num_filters, channel_wise=channel_wise)(input)
     
     return input
 
 def Unet(pretrained_weights=None, input_size=(None, None, 3), num_filters=32, 
-         use_deform=True, normal_conv_trainable=True):
+         use_deform=True, channel_wise=False, normal_conv_trainable=True):
+    
+    global Conv
+    Conv = partial(Conv, normal_conv_trainable=normal_conv_trainable,
+                   use_deform=use_deform, channel_wise=channel_wise)
     
     inputs = Input(input_size)
     
     input = Input(input_size)
     
-    conv1 = Conv(input, num_filters, use_deform=False, 
-                 normal_conv_trainable=normal_conv_trainable)
-    conv1 = Conv(conv1, num_filters, use_deform=False, 
-                 normal_conv_trainable=normal_conv_trainable)
+    conv1 = Conv(input, num_filters, use_deform=False)
+    conv1 = Conv(conv1, num_filters, use_deform=False)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
     
-    conv2 = Conv(pool1, num_filters*2, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
-    conv2 = Conv(conv2, num_filters*2, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
+    conv2 = Conv(pool1, num_filters*2)
+    conv2 = Conv(conv2, num_filters*2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
     
-    conv3 = Conv(pool2, num_filters*4, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
-    conv3 = Conv(conv3, num_filters*4, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
+    conv3 = Conv(pool2, num_filters*4)
+    conv3 = Conv(conv3, num_filters*4)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
     
-    conv4 = Conv(pool3, num_filters*8, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
-    conv4 = Conv(conv4, num_filters*8, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
+    conv4 = Conv(pool3, num_filters*8)
+    conv4 = Conv(conv4, num_filters*8)
     drop4 = Dropout(0.5)(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
     
-    conv5 = Conv(pool4, num_filters*16, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
-    conv5 = Conv(conv5, num_filters*16, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
+    conv5 = Conv(pool4, num_filters*16)
+    conv5 = Conv(conv5, num_filters*16)
     drop5 = Dropout(0.5)(conv5)
     
-    up6 = Conv(UpSampling2D(size = (2,2))(drop5), num_filters*8, use_deform, 
-               normal_conv_trainable=normal_conv_trainable)
+    up6 = Conv(UpSampling2D(size = (2,2))(drop5), num_filters*8)
     merge6 = concatenate([drop4, up6], axis=3)
-    conv6 = Conv(merge6, num_filters*8, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
-    conv6 = Conv(conv6, num_filters*8, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
+    conv6 = Conv(merge6, num_filters*8)
+    conv6 = Conv(conv6, num_filters*8)
     
-    up7 = Conv(UpSampling2D(size = (2,2))(conv6), num_filters*4, use_deform, 
-               normal_conv_trainable=normal_conv_trainable)
+    up7 = Conv(UpSampling2D(size = (2,2))(conv6), num_filters*4)
     merge7 = concatenate([conv3, up7], axis=3)
-    conv7 = Conv(merge7, num_filters*4, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
-    conv7 = Conv(conv7, num_filters*4, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
+    conv7 = Conv(merge7, num_filters*4)
+    conv7 = Conv(conv7, num_filters*4)
     
-    up8 = Conv(UpSampling2D(size = (2,2))(conv7), num_filters*2, use_deform, 
-               normal_conv_trainable=normal_conv_trainable)
+    up8 = Conv(UpSampling2D(size = (2,2))(conv7), num_filters*2)
     merge8 = concatenate([conv2, up8], axis=3)
-    conv8 = Conv(merge8, num_filters*2, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
-    conv8 = Conv(conv8, num_filters*2, use_deform, 
-                 normal_conv_trainable=normal_conv_trainable)
+    conv8 = Conv(merge8, num_filters*2)
+    conv8 = Conv(conv8, num_filters*2)
     
-    up9 = Conv(UpSampling2D(size = (2,2))(conv8), num_filters, use_deform, 
-               normal_conv_trainable=normal_conv_trainable)
+    up9 = Conv(UpSampling2D(size = (2,2))(conv8), num_filters)
     merge9 = concatenate([conv1, up9], axis=3)
-    conv9 = Conv(merge9, num_filters, use_deform=False, 
-                 normal_conv_trainable=normal_conv_trainable)
-    conv9 = Conv(conv9, num_filters, use_deform=False, 
-                 normal_conv_trainable=normal_conv_trainable)
+    conv9 = Conv(merge9, num_filters, use_deform=False)
+    conv9 = Conv(conv9, num_filters, use_deform=False)
     
     key_mask = Conv2D(1, 1, activation='sigmoid', name='key_mask', 
                       trainable=normal_conv_trainable)(conv9)
@@ -151,6 +132,7 @@ def Unet(pretrained_weights=None, input_size=(None, None, 3), num_filters=32,
     
     return model
 
+# import random
 # h = 64
 # w = 96
 # n = 10
@@ -161,19 +143,3 @@ def Unet(pretrained_weights=None, input_size=(None, None, 3), num_filters=32,
 # 
 # model.fit(X, y, epochs=1)
 
-
-model = Unet(input_size=(None, None, 3), num_filters=4, use_deform=True,
-             normal_conv_trainable=True)
-# def gen():
-#     while True:
-#         h = round_up_dividend(random.randint(64, 256), 16)
-#         w = round_up_dividend(random.randint(64, 256), 16)
-#         x = np.random.rand(1, h, w, 1)
-#         y = (np.random.rand(1, h, w, 1) > 0.5) * 1.0
-#         yield x, y
-
-model.fit_generator(data_generator('dataset/training_data', 2/3, shuffle=True), 
-                    steps_per_epoch=99, 
-                    validation_data=data_generator('dataset/training_data', -1/3),
-                    validation_steps=50,
-                    epochs=100)
---
+++ train.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import click
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+
+from deform_unet import Unet
+from load_data import data_generator
+
+@click.command()
+@click.option('--pretrained_weights', '-w', default=None)
+@click.option('--checkpoint_dir', '-s', default='checkpoint/')
+@click.option('--deform/--no-deform', '-D/-nD', 'use_deform', default=True)
+@click.option('--train-norm-conv/--no-train-norm-conv', '-F/-nF', 'normal_conv_trainable', default=True)
+@click.option('--deform-channel/--no-deform-channel', '-C/-nC', 'channel_wise', default=False)
+def train(pretrained_weights, checkpoint_dir, use_deform, channel_wise,
+          normal_conv_trainable):
+    
+    checkpoint_dir = Path(checkpoint_dir)
+    checkpoint_dir.mkdir(parents=True, exist_ok=True)
+    ckpt_path = checkpoint_dir / 'ep{epoch:03d}_key-iou{val_key_mask_IoU_score:.4f}_value-iou{val_value_mask_IoU_score:.4f}.h5'
+    checkpoint = ModelCheckpoint(ckpt_path, monitor='val_loss', 
+                                 save_weights_only=True, save_best_only=True, 
+                                 verbose=1)
+
+    model = Unet(pretrained_weights, input_size=(None, None, 3), num_filters=4, 
+                 use_deform=use_deform, channel_wise=channel_wise, 
+                 normal_conv_trainable=normal_conv_trainable)
+
+    model.fit_generator(data_generator('dataset/training_data', 2/3, shuffle=True), 
+                        steps_per_epoch=99, 
+                        validation_data=data_generator('dataset/training_data', -1/3),
+                        validation_steps=50,
+                        epochs=100)
+
+    model.evaluate_generator(data_generator('dataset/testing_data'), steps=50)
+    
+    return model
+
+if __name__ == '__main__':
+    model = train()
